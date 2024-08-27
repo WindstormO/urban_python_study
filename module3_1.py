8eddb9c762194e0f847e3cfd8711794f0a0575ec
Artyom Pershin <artempershin@inbox.ru>
2024-08-28 02:13:42 +0300

    Update module3_1.py
    Добавлены примеры входящих данных согласно образцу

--- module3_1.py
+++ module3_1.py
@@ -11,4 +11,8 @@ def is_contains(string, list_to_search):
   count_calls()
 
 
-print(f"Количество вызванных функций: {calls}")
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
