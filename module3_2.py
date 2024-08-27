15892b8116f8387ce4e21b9a071deabbfc55417c
WindstormP <artempershin@inbox.ru>
2024-08-28 01:20:37 +0300

    Update module3_2.py

--- module3_2.py
+++ module3_2.py
@@ -1,4 +1,4 @@
-def email_check(sender="university.help@gmail.com", recipient=""):
+def email_check(sender="", recipient=""):
     if not "@" in sender:
         return(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
     elif sender[-3:]==".ru":
@@ -11,4 +11,19 @@ def email_check(sender="university.help@gmail.com", recipient=""):
         return(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
 
 
-print(email_check())
+def send_email(message="", sender="university.help@gmail.com", recipient=""):
+    if bool(email_check(sender, recipient)):
+        print(email_check)
+    else:
+        if recipient==sender:
+            return(f"Нельзя отправить письмо самому себе!")
+        elif sender=="university.help@gmail.com":
+            return(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}.")
+        else:
+            return(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
+
+
+print(send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com'))
+print(send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com'))
+print(send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk'))
+print(send_email(('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru'))
