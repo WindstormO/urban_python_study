3ce481f0737042c3f222ad3f715a12628cedeeb8
Artyom Pershin <artempershin@inbox.ru>
2024-08-30 01:47:21 +0300

    Update module3_1.py
    Правильно написаны функции count_calls, string_info

--- module3_1.py
+++ module3_1.py
@@ -1,18 +1,36 @@
 calls = 0
 def count_calls():
-  global calls+=1
+    global calls
+    calls+=1
+    
+
+def string_info(string="String"):
+    count_calls()
+    print(len(string))
+    string_tuple=(len(string), string.lower(), string.upper())
+    return string_tuple
 
 
-def string_info(string):
+
+print(string_info())
+
+
+
+def is_contains(content="AaA", list_to_search=["AAA"]):
   count_calls()
+  for i in list_to_search:
+      print(list_to_search[i])
+      #if list_to_search[i].upper() == content:
+          #print({EQ})
 
 
-def is_contains(string, list_to_search):
-  count_calls()
+
+is_contains()
 
 
-print(string_info('Capybara'))
-print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
-print(calls)
+#print(string_info('Capybara'))
+#print(string_info('Armageddon'))
+#print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+#print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+#print(calls)
+
