315a1d3e4f3fb78cd2e5ebe24cb3c62797ab9d43
Artyom Pershin <artempershin@inbox.ru>
2024-08-30 08:41:23 +0300

    Update module3_4.py
    Финальная версия с учётом регистра

--- module3_4.py
+++ module3_4.py
@@ -3,9 +3,8 @@ def single_root_words(root_word, *other_words):
     same_words=[]
     print(f"root_word:{root_word} , other_words: {other_words}")
     for words in range(len(other_words)):
-        if root_word in other_words[words] or other_words[words] in root_word:
+        if root_word.lower() in other_words[words].lower() or other_words[words].lower() in root_word.lower():
             same_words.append(other_words[words])
-            same_words.append(root_word)
     return same_words
 
 
