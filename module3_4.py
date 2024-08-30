aa21db3c227c378b573bd6083a56814710b906ae
Artyom Pershin <artempershin@inbox.ru>
2024-08-30 08:39:10 +0300

    Update module3_4.py
    Внесены исправления. Всё работает. Осталось сделать так, чтобы "root word" добавлялось только в единичном экземпляре

--- module3_4.py
+++ module3_4.py
@@ -1,11 +1,11 @@
-def single_root_words(root_words, *other_words):
+def single_root_words(root_word, *other_words):
     """Функция, определяющая однокоренные слова"""
     same_words=[]
+    print(f"root_word:{root_word} , other_words: {other_words}")
     for words in range(len(other_words)):
-        if root_words in other_words:
-            same_words.append(root_words)
-        elif other_words[words] in root_words:
-            same_words.append(root_words)
+        if root_word in other_words[words] or other_words[words] in root_word:
+            same_words.append(other_words[words])
+            same_words.append(root_word)
     return same_words
 
 
@@ -13,4 +13,3 @@ result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 print(result1)
 print(result2)
-        
