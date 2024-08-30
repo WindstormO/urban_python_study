8d43463b59fd73d55b9061a1aa33dbf39c17a041
Artyom Pershin <artempershin@inbox.ru>
2024-08-30 05:32:52 +0300

    Update module3_4.py
    Версия, написанная "с ходу"

--- module3_4.py
+++ module3_4.py
@@ -1,8 +1,16 @@
-def single_root_words(root_word, *other_words):
-  same_words=[]
-  for i in other_words:
-    if other_words==root_word:
-      same_words.append(other_words)
-      return same_words
+def single_root_words(root_words, *other_words):
+"""Функция, определяющая однокоренные слова"""
+    same_words=[]
+    for words in range(other_words):
+        if root_words in other_words:
+            same_words.append(root_words)
+        elif other_words in root_words:
+            same_words.append(root_words)
+    return same_words
 
-  
+
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
+print(result1)
+print(result2)
+        
